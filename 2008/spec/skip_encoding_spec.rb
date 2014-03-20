b054807f04f30660c6b1612c5c24e9e062f1835f
ACEfanatic02 <acefanatic02@gmail.com>
2014-03-20 13:34:03 -0400

    more tests for skip encoding

--- 2008/spec/skip_encoding_spec.rb
+++ 2008/spec/skip_encoding_spec.rb
@@ -18,5 +18,25 @@ describe SkipDecoder do
       sd = SkipDecoder.new
       sd.find("foo", "faoaao").should be_nil
     end
+
+    it 'finds a string in itself' do
+      sd = SkipDecoder.new
+      sd.find('foo', 'foo').should eq 0
+    end
+
+    it 'does not find a string in wrong case' do
+      sd = SkipDecoder.new
+      sd.find('Foo', 'faaoaao').should be_nil
+    end
+  end
+
+  describe '#test' do
+
+    it 'returns whether a given n produces the needle from the haystack' do
+      sd = SkipDecoder.new
+      sd.test('foo', 'foo', 0).should be_true
+      sd.test('foo', 'faaoaao', 1).should be_false
+      sd.test('foo', 'faaoaao', 2).should be_true
+    end
   end
 end
